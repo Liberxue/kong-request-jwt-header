75da37685db64ecbe15b5254d5e02000b7e54fc7
Liberxue <liberxue@gmail.com>
2022-03-25 15:37:07 +0800

    remove debug code

--- request-jwt-header/handler.lua
+++ request-jwt-header/handler.lua
@@ -1,29 +1,3 @@
--- local BasePlugin = require "kong.plugins.base_plugin"
--- local SetRequestJWTHeader = BasePlugin:extend()
--- local jwt = require "luajwt"
-
--- SetRequestJWTHeader.VERSION = "0.1.0"
--- SetRequestJWTHeader.PRIORITY = 2000
-
--- -- local SetRequestJWTHeader = {
--- --   PRIORITY = 900,
--- --   VERSION = "1.0"
--- -- }
--- local payload = {
--- 	iss = "12345678",
--- 	nbf = os.time(),
--- 	exp = os.time() + 8400,
--- }
-
--- -- -- encode
--- local alg = "HS256" -- (default)
--- local token, err = jwt.encode(payload,get_jwt_secret, alg)
--- function SetRequestJWTHeader:rewrite(conf)
---    kong.service.request.set_header("X-Kong-JWT-Kong", token)
---    kong.log("saying hi from the 'rewrite' handler")
--- end
-
--- return SetRequestJWTHeader
 local BasePlugin = require "kong.plugins.base_plugin"
 local responses = require "kong.tools.responses"
 local jwt = require "resty.jwt"
